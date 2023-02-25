88d70bf4348f603f6e562f5604fffa759a5fdd67
macbookpro545 <senan270622@gmail.com>
2023-02-25 08:07:00 +0400

    * .gitignore: Degiskenler Yaratildi
    * OdevPINPUKrepo.sln:
    * Program.cs:
    * OdevPINPUKrepo.csproj:
    * OdevPINPUKrepo.csproj.user:
    * AssemblyInfo.cs:

---
+++ OdevPINPUKrepo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OdevPINPUKrepo
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("\tSenan'ın Ödevi\n");
+            int dogruPIN = 1996;
+            int dogruPUK = 1405;
+            int girilenPIN = 0;
+            int girilenPUK = 0;
+        }
+    }
+}
