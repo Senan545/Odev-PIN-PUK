2fe3e3d965826adaf8f5cd83754776dd68cf1eb3
macbookpro545 <senan270622@gmail.com>
2023-02-25 08:14:58 +0400

    PIN kod kismi yazildi

--- OdevPINPUKrepo/Program.cs
+++ OdevPINPUKrepo/Program.cs
@@ -11,6 +11,39 @@ namespace OdevPINPUKrepo
             int dogruPUK = 1405;
             int girilenPIN = 0;
             int girilenPUK = 0;
+
+            for (int PINhakki = 2; PINhakki >= 0; PINhakki--)
+            {
+                Console.Write("Telefonunu açmak için PIN Kodunu Giriniz: ");
+                girilenPIN = int.Parse(Console.ReadLine());
+
+                if (girilenPIN != dogruPIN)
+                {
+                    if (PINhakki == 0)
+                    {
+                        Console.Write("Yanlış PIN Girdiniz, PIN Hakkınız Kalmadı.\n");
+
+                       
+                    }
+                    else
+                    {
+                        Console.WriteLine("Yanlış PIN Girdiniz. {0} Hakkınız Kaldı\n", PINhakki);
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+            if (girilenPIN == dogruPIN || girilenPUK == dogruPUK)
+            {
+                Console.WriteLine("Telefon Açılıyor...");
+            }
+            else
+            {
+                Console.WriteLine("\nTelefonu Açmak için Müşteri Hizmetlerine Gidiniz.");
+            }
+
         }
     }
 }
